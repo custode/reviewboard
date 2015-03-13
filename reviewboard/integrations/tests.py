111b3f383e44f35959b1a191206a79619bebc18f
xuanyi <xuanyi.custode@gmail.com>
2015-03-13 14:56:50 +0800

    Fix unit test docstring and arrange import in alphebatical order

--- reviewboard/integrations/tests.py
+++ reviewboard/integrations/tests.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 
-from reviewboard.testing.testcase import TestCase
-from reviewboard.integrations.models import ConfiguredIntegration
 from reviewboard.integrations.integration import (Integration,
                                                   register_integration,
                                                   unregister_integration)
+from reviewboard.integrations.models import ConfiguredIntegration
 from reviewboard.integrations.manager import IntegrationManager
+from reviewboard.testing.testcase import TestCase
 
 
 class TestIntegration(Integration):
@@ -13,7 +13,7 @@ class TestIntegration(Integration):
 
 
 class IntegrationManagerTest(TestCase):
-    """Testing Integration Manager"""
+    """Testing Integration Manager."""
 
     def create_configured_integration(self, integration_id="TestIntegration",
                                       is_enabled=False,
@@ -43,9 +43,8 @@ class IntegrationManagerTest(TestCase):
         unregister_integration(TestIntegration)
 
     def test_start_with_existing_configs(self):
-        """
-        Testing integration manager initializing with existing configured
-        integrations"""
+        """Testing integration manager initializing with existing configured
+        integrations."""
         config1 = self.create_configured_integration()
         config2 = self.create_configured_integration(is_enabled=True)
         self.manager.register_config(config1)
@@ -58,27 +57,22 @@ class IntegrationManagerTest(TestCase):
         self.assertIn(config, configs)
 
     def test_start_with_empty_configs(self):
-        """
-        Testing integration manager initializing with no configured
-        integrations
-        """
+        """Testing integration manager initializing with no configured
+        integrations."""
         configs = self.manager.get_config_instances()
         self.assertEqual(len(configs), 0)
 
     def test_register_duplicate_config(self):
-        """
-        Testing integration manager registering duplicate configured
-        integration
-        """
+        """Testing integration manager registering duplicate configured
+        integration."""
         config1 = self.create_configured_integration()
         self.manager.register_config(config1)
 
         self.assertRaises(KeyError, self.manager.register_config, config1)
 
     def test_register_duplicate_config_with_reregister(self):
-        """
-        Testing integration manager updating registered configured integration
-        """
+        """Testing integration manager updating registered configured
+        integration."""
         config1 = self.create_configured_integration()
         config2 = self.create_configured_integration(is_enabled=True)
         self.manager.register_config(config1)
@@ -88,10 +82,8 @@ class IntegrationManagerTest(TestCase):
         self.assertEqual(len(self.manager.get_config_instances()), 2)
 
     def test_unregister_exisiting_config(self):
-        """
-        Testing integration manager unregistering registered configured
-        integration
-        """
+        """Testing integration manager unregistering registered configured
+        integration."""
         config1 = self.create_configured_integration()
         config2 = self.create_configured_integration(is_enabled=True)
         self.manager.register_config(config1)
@@ -103,15 +95,12 @@ class IntegrationManagerTest(TestCase):
         self.assertNotIn(config1, self.manager.get_config_instances())
 
     def test_unregister_non_existent_config(self):
-        """
-        Testing integration manager unregistering nonexistent configured
-        integration
-        """
+        """Testing integration manager unregistering nonexistent configured
+        integration."""
         self.assertRaises(KeyError, self.manager.unregister_config, 3)
 
     def test_toggle_config(self):
-        """Testing integration manager toggling of configured integration"""
-
+        """Testing integration manager toggling of configured integration."""
         config1 = self.create_configured_integration()
         self.manager.register_config(config1)
 
