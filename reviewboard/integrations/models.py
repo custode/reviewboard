fd9f279bc208e77f00053db5a3b15417ed8872f5
xuanyi <xuanyi.custode@gmail.com>
2015-03-13 17:23:30 +0800

    Fix issue with integration model

--- reviewboard/integrations/models.py
+++ reviewboard/integrations/models.py
@@ -1,14 +1,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 from djblets.db.fields import JSONField
 
-from reviewboard.site.models import LocalSite
 from reviewboard.integrations.integration import get_integration
+from reviewboard.site.models import LocalSite
 
 
 class ConfiguredIntegration(models.Model):
+    """Configuration of an intgration."""
+
     integration_id = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True)
     is_enabled = models.BooleanField(default=False)
@@ -19,29 +22,26 @@ class ConfiguredIntegration(models.Model):
                                    blank=True,
                                    null=True)
 
-    @property
+    @cached_property
     def integration(self):
-        if not hasattr(self, '_integration'):
-            cls = get_integration(self.integration_id)
+        """The integration instance for this configuration."""
+        cls = get_integration(self.integration_id)
 
-            if cls:
-                self._integration = cls(self)
-            else:
-                self._integration = None
-
-        return self._integration
+        if cls:
+            return cls(self)
+        else:
+            return None
 
     def is_accessible_by(self, user):
-        """Returns whether or not the user has access to the integration.
+        """Return whether or not the user has access to the integration.
 
         The integration can be access by the user if it is set to global or if
-        the user have access to the LocalSite.
+        the user has access to the LocalSite.
         """
         return not self.local_site or self.local_site.is_accessible_by(user)
 
     def is_mutable_by(self, user):
-        """Return whether or not the user can modify the configuration of
-        the integration.
+        """Return whether the user can modify this configuration.
 
         The integration can be change by an administrator of the global site
         with the proper permission or the administrator of the LocalSite.
