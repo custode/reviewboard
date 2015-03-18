f3142293259c746c5369f63766edb8520c8bba7a
xuanyi <xuanyi.custode@gmail.com>
2015-03-18 16:28:58 +0800

    Clean up views for integration

--- reviewboard/integrations/views.py
+++ reviewboard/integrations/views.py
@@ -15,17 +15,37 @@ manager = get_integration_manager()
 @staff_member_required
 def integration_list(request,
                      template_name='integrations/integration_list.html'):
-    if request.method == 'POST':
-        if 'full-reload' in request.POST:
-            # Reload both extension manager and integration manager
-            return HttpResponseRedirect('.')
-    else:
-        # manager.load()
         return render_to_response(template_name, RequestContext(request))
 
 
 @csrf_protect
 @staff_member_required
-def configure_integration(request, form_class,
-                          template_name='integrations/configure_integration.html'):
-    pass
+def configure_integration(request, integration_class=None, config_id=None,
+                          template_name='integrations/configure_integration'
+                                        '.html'):
+    if config_id:
+        config_instance = manager.get_config_instance(int(config_id))
+        integration_class = config_instance.integration.integration_id
+
+    if integration_class:
+        integration = manager.get_integration(integration_class)
+
+    if not integration:
+        raise Http404
+    else:
+        form_class = integration.form
+
+    if request.method == 'POST':
+        form = form_class(integration, request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect(request.path + '?save=1')
+    else:
+        form = form_class(integration, config=config_instance)
+
+    return render_to_response(template_name, RequestContext(request, {
+        'integration_name': integration.name,
+        'form': form,
+        }))
