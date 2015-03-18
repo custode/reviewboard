3bad072ab3443fa51cabbf66ceb2f013800afccd
xuanyi <xuanyi.custode@gmail.com>
2015-03-18 14:07:23 +0800

    Convert to 4 space for js file

--- reviewboard/static/rb/js/integrations/models/integrationManagerModel.js
+++ reviewboard/static/rb/js/integrations/models/integrationManagerModel.js
@@ -2,189 +2,189 @@
 This represent an integration model
 */
 Integration = Backbone.Model.extend({
-  defaults: {
-    integrationID: null,
-    name: null,
-    description: null,
-    iconPath: null,
-    newLink: null
-  },
+	defaults: {
+		integrationID: null,
+		name: null,
+		description: null,
+		iconPath: null,
+		newLink: null
+	},
 
-  parse: function(rsp) {
-    return {
-      integrationID: rsp.integration_id,
-      name: rsp.name,
-      description: rsp.description,
-      iconPath: rsp.icon_path,
-      newLink: rsp.new_link
-    };
-  }
+	parse: function(rsp) {
+		return {
+			integrationID: rsp.integration_id,
+			name: rsp.name,
+			description: rsp.description,
+			iconPath: rsp.icon_path,
+			newLink: rsp.new_link
+		};
+	}
 });
 
 /*
 This represent an configured integration model
 */
 ConfiguredIntegration = Backbone.Model.extend({
-  defaults: {
-    id: null,
-    name: null,
-    integration: null,
-    integrationDescription: null,
-    integrationIcon: null,
-    enabled: null,
-    configuration: null,
-    configureLink: null
-  },
+	defaults: {
+		id: null,
+		name: null,
+		integration: null,
+		integrationDescription: null,
+		integrationIcon: null,
+		enabled: null,
+		configuration: null,
+		configureLink: null
+	},
 
-  url: function() {
-    return SITE_ROOT + 'api/configured-integrations/' + this.id + '/';
-  },
+	url: function() {
+		return SITE_ROOT + 'api/configured-integrations/' + this.id + '/';
+	},
 
-  /*
-   * Enables the integration.
-   */
-  enable: function() {
-      this.save({
-          enabled: true
-      }, {
-          wait: true,
-          error: _.bind(function(model, xhr) {
-              console.log("error");
-              this.set({
-                  loadable: false,
-                  loadError: xhr.errorRsp.load_error,
-                  canEnable: !xhr.errorRsp.needs_reload
-              });
-          }, this)
-      });
-  },
+	/*
+	 * Enables the integration.
+	 */
+	enable: function() {
+			this.save({
+					enabled: true
+			}, {
+					wait: true,
+					error: _.bind(function(model, xhr) {
+							console.log("error");
+							this.set({
+									loadable: false,
+									loadError: xhr.errorRsp.load_error,
+									canEnable: !xhr.errorRsp.needs_reload
+							});
+					}, this)
+			});
+	},
 
-  /*
-   * Disables the integration.
-   */
-  disable: function() {
-      this.save({
-          enabled: false
-      }, {
-          wait: true,
-          error: function(model, xhr) {
-              alert(gettext('Failed to disable integration. ') +
-                    xhr.errorText + '.');
-          }
-      });
-  },
+	/*
+	 * Disables the integration.
+	 */
+	disable: function() {
+			this.save({
+					enabled: false
+			}, {
+					wait: true,
+					error: function(model, xhr) {
+							alert(gettext('Failed to disable integration. ') +
+										xhr.errorText + '.');
+					}
+			});
+	},
 
-  /*
-   * Returns a JSON payload for requests sent to the server.
-   */
-  toJSON: function() {
-      return {
-          enabled: this.get('enabled')
-      };
-  },
+	/*
+	 * Returns a JSON payload for requests sent to the server.
+	 */
+	toJSON: function() {
+			return {
+					enabled: this.get('enabled')
+			};
+	},
 
-  /*
-   * Performs AJAX requests against the server-side API.
-   */
-  sync: function(method, model, options) {
-      Backbone.sync.call(this, method, model, _.defaults({
-          contentType: 'application/x-www-form-urlencoded',
-          data: model.toJSON(options),
-          processData: true,
-          error: _.bind(function(xhr) {
-              var rsp = null,
-                  loadError,
-                  text;
+	/*
+	 * Performs AJAX requests against the server-side API.
+	 */
+	sync: function(method, model, options) {
+			Backbone.sync.call(this, method, model, _.defaults({
+					contentType: 'application/x-www-form-urlencoded',
+					data: model.toJSON(options),
+					processData: true,
+					error: _.bind(function(xhr) {
+							var rsp = null,
+									loadError,
+									text;
 
-              try {
-                  rsp = $.parseJSON(xhr.responseText);
-                  text = rsp.err.msg;
-                  loadError = rsp.load_error;
-              } catch (e) {
-                  text = 'HTTP ' + xhr.status + ' ' + xhr.statusText;
-              }
+							try {
+									rsp = $.parseJSON(xhr.responseText);
+									text = rsp.err.msg;
+									loadError = rsp.load_error;
+							} catch (e) {
+									text = 'HTTP ' + xhr.status + ' ' + xhr.statusText;
+							}
 
-              if (_.isFunction(options.error)) {
-                  xhr.errorText = text;
-                  xhr.errorRsp = rsp;
-                  options.error(xhr, options);
-              }
-          }, this)
-      }, options));
-  },
+							if (_.isFunction(options.error)) {
+									xhr.errorText = text;
+									xhr.errorRsp = rsp;
+									options.error(xhr, options);
+							}
+					}, this)
+			}, options));
+	},
 
-  parse: function(rsp) {
-    if (rsp.stat !== undefined) {
-      rsp = rsp.configured_integration;
-    }
+	parse: function(rsp) {
+		if (rsp.stat !== undefined) {
+			rsp = rsp.configured_integration;
+		}
 
-    return {
-      id: rsp.id,
-      name: rsp.name,
-      integration: rsp.integration_id,
-      integrationDescription: rsp.integration_description,
-      integrationIcon: rsp.integration_icon,
-      description: rsp.description,
-      enabled: rsp.is_enabled,
-      configuration: rsp.configuration,
-      configureLink: rsp.configure_link
-    };
-  }
+		return {
+			id: rsp.id,
+			name: rsp.name,
+			integration: rsp.integration_id,
+			integrationDescription: rsp.integration_description,
+			integrationIcon: rsp.integration_icon,
+			description: rsp.description,
+			enabled: rsp.is_enabled,
+			configuration: rsp.configuration,
+			configureLink: rsp.configure_link
+		};
+	}
 });
 
 IntegrationCollection = Backbone.Collection.extend({
-  model: Integration,
+	model: Integration,
 
-  url: function() {
-    return SITE_ROOT + 'api/integrations/';
-  },
+	url: function() {
+		return SITE_ROOT + 'api/integrations/';
+	},
 
-  parse: function(rsp) {
-    return rsp.integrations;
-  }
+	parse: function(rsp) {
+		return rsp.integrations;
+	}
 });
 
 ConfiguredIntegrationCollection = Backbone.Collection.extend({
-  model: ConfiguredIntegration,
+	model: ConfiguredIntegration,
 
-  initialize: function(options) {
-    this.integrationID = options.integrationID;
-  },
+	initialize: function(options) {
+		this.integrationID = options.integrationID;
+	},
 
-  url: function() {
-    return SITE_ROOT + 'api/configured-integrations/?integrationID=' +
-      this.integrationID;
-  },
+	url: function() {
+		return SITE_ROOT + 'api/configured-integrations/?integrationID=' +
+			this.integrationID;
+	},
 
-  parse: function(rsp) {
-    return rsp.configured_integrations;
-  }
+	parse: function(rsp) {
+		return rsp.configured_integrations;
+	}
 });
 
 IntegrationManager = Backbone.Model.extend({
-  initialize: function() {
-    this.integrations = new IntegrationCollection();
-  },
+	initialize: function() {
+		this.integrations = new IntegrationCollection();
+	},
 
-  load: function() {
-    this.integrations.fetch({
-      success: _.bind(function() {
-        this.trigger('loaded');
-      }, this)
-    });
-  }
+	load: function() {
+		this.integrations.fetch({
+			success: _.bind(function() {
+				this.trigger('loaded');
+			}, this)
+		});
+	}
 });
 
 ConfiguredIntegrationManager = Backbone.Model.extend({
-  initialize: function(options) {
-    this.configuredIntegrations = new ConfiguredIntegrationCollection(options);
-  },
+	initialize: function(options) {
+		this.configuredIntegrations = new ConfiguredIntegrationCollection(options);
+	},
 
-  load: function() {
-    this.configuredIntegrations.fetch({
-      success: _.bind(function() {
-        this.trigger('loaded');
-      }, this)
-    });
-  }
+	load: function() {
+		this.configuredIntegrations.fetch({
+			success: _.bind(function() {
+				this.trigger('loaded');
+			}, this)
+		});
+	}
 });
--- reviewboard/static/rb/js/integrations/views/integrationManagerView.js
+++ reviewboard/static/rb/js/integrations/views/integrationManagerView.js
@@ -2,177 +2,162 @@
 Provide the view for each integration
 */
 IntegrationView = Backbone.View.extend({
-  className: 'integration',
-  tagName: 'li',
+	className: 'integration',
+	tagName: 'li',
 
-  events: {
-    'click .manage-integration': '_toggleManage',
-    'click .add-new': '_addNewIntegration'
-  },
+	events: {
+		'click .add-new': '_addNewIntegration'
+	},
 
-  template: _.template([
-    '<div class="integration-header">',
-    '<img class="integration-icon" src="<%-iconPath%>">',
-    ' <div class="integration-content"><h1><%- name %></h1>',
-    ' <div class="description"><%- description %></div>',
-    ' <ul class="expand">',
-    '   <li class="manage-integration"><a href="#">Add</a></li>',
-    ' </ul></div>',
-    '</div>',
-    '<ul class="configured-integrations">',
-    '</ul>'
-  ].join('')),
+	template: _.template([
+		'<div class="integration-header">',
+		'<img class="integration-icon" src="<%-iconPath%>">',
+		' <div class="integration-content"><h1><%- name %></h1>',
+		' <div class="description"><%- description %></div>',
+		' <ul class="expand">',
+		'   <li><a href="#">Add</a></li>',
+		' </ul></div>',
+		'</div>',
+		'<ul class="configured-integrations">',
+		'</ul>'
+	].join('')),
 
-  initialize: function() {
-    this._$configuredIntegrationsView = null;
-    this.toggle = false;
-  },
+	initialize: function() {
+		this._$configuredIntegrationsView = null;
+		this.toggle = false;
+	},
 
-  render: function() {
-    this.$el.html(this.template(this.model.attributes));
+	render: function() {
+		this.$el.html(this.template(this.model.attributes));
 
-    this._toggleManage();
-  },
+		this._loadConfiguredIntegrations();
+	},
 
-  _toggleManage: function() {
-    if (this._$configuredIntegrationsView === null) {
-      this._loadConfiguredIntegrations();
-    }
+	_loadConfiguredIntegrations: function() {
+		this._$configuredIntegrationsView = new ConfiguredIntegrationManagerView({
+			el: this.$el,
+			model: new ConfiguredIntegrationManager({
+				integrationID: this.model.attributes.integrationID
+			})
+		});
 
-    this.$('.configured-integrations').toggle('fast');
-
-    this.$('.manage-integration div')
-      .removeClass(this.toggle ? 'down' : 'right')
-      .addClass(this.toggle ? 'right' : 'down');
-
-    this.toggle = this.toggle ? false : true;
-  },
-
-  _loadConfiguredIntegrations: function() {
-    this._$configuredIntegrationsView = new ConfiguredIntegrationManagerView({
-      el: this.$el,
-      model: new ConfiguredIntegrationManager({
-        integrationID: this.model.attributes.integrationID
-      })
-    });
-
-    this._$configuredIntegrationsView.render();
-  }
+		this._$configuredIntegrationsView.render();
+	}
 });
 
 /*
 Provide the view for each configured integration
 */
 ConfiguredIntegrationView = Backbone.View.extend({
-  className: 'configured-integration',
-  tagName: 'li',
+	className: 'configured-integration',
+	tagName: 'li',
 
-  events: {
-    'click .configure-integration': '_configureIntegration',
-    'click .enable-toggle': '_toggleEnableState'
-  },
+	events: {
+		'click .configure-integration': '_configureIntegration',
+		'click .enable-toggle': '_toggleEnableState'
+	},
 
-  template: _.template([
-    '<div class="configured-header">',
-    ' <div class="description"><%- description %></div>',
-    ' <ul class="options">',
-    '   <li class="option"><a href="#" class="enable-toggle"></a></li>',
-    '   <li class="option">',
-    '     <a href="<%- configureLink %>" class="configure-integration">',
-    '        Configure',
-    '     </a>',
-    '   </li>',
-    ' </ul>',
-    '</div>'
-  ].join('')),
+	template: _.template([
+		'<div class="configured-header">',
+		' <div class="description"><%- description %></div>',
+		' <ul class="options">',
+		'   <li class="option"><a href="#" class="enable-toggle"></a></li>',
+		'   <li class="option">',
+		'     <a href="<%- configureLink %>" class="configure-integration">',
+		'        Configure',
+		'     </a>',
+		'   </li>',
+		' </ul>',
+		'</div>'
+	].join('')),
 
-  render: function() {
-    this.$el.html(this.template(this.model.attributes));
+	render: function() {
+		this.$el.html(this.template(this.model.attributes));
 
-    this._$enableToggle = this.$('.enable-toggle');
-    this.listenTo(this.model, 'change:enabled', this._showEnabledState);
-    this._showEnabledState();
-  },
+		this._$enableToggle = this.$('.enable-toggle');
+		this.listenTo(this.model, 'change:enabled', this._showEnabledState);
+		this._showEnabledState();
+	},
 
-  _showEnabledState: function() {
-    var enabled = this.model.get('enabled');
+	_showEnabledState: function() {
+		var enabled = this.model.get('enabled');
 
-    this.$el
-      .removeClass(enabled ? 'disabled' : 'enabled')
-      .addClass(enabled ? 'enabled' : 'disabled');
+		this.$el
+			.removeClass(enabled ? 'disabled' : 'enabled')
+			.addClass(enabled ? 'enabled' : 'disabled');
 
-    this._$enableToggle
-      .text(enabled ? gettext('Disable') : gettext('Enable'));
-  },
+		this._$enableToggle
+			.text(enabled ? gettext('Disable') : gettext('Enable'));
+	},
 
-  _toggleEnableState: function() {
-    if (this.model.get('enabled')) {
-      this.model.disable();
-    } else {
-      this.model.enable();
-    }
+	_toggleEnableState: function() {
+		if (this.model.get('enabled')) {
+			this.model.disable();
+		} else {
+			this.model.enable();
+		}
 
-    return false;
-  }
+		return false;
+	}
 });
 
 /*
 */
 IntegrationManagerView = Backbone.View.extend({
-  initialize: function() {
-    this._$integrations = null;
-  },
+	initialize: function() {
+		this._$integrations = null;
+	},
 
-  render: function() {
-    this._$integrations = this.$('.integrations');
+	render: function() {
+		this._$integrations = this.$('.integrations');
 
-    this.listenTo(this.model, 'loaded', this._onLoaded);
+		this.listenTo(this.model, 'loaded', this._onLoaded);
 
-    this.model.load();
+		this.model.load();
 
-    return this;
-  },
+		return this;
+	},
 
-  _onLoaded: function() {
-    this.model.integrations.each(function(integration) {
-      var view = new IntegrationView({
-        model: integration
-      });
+	_onLoaded: function() {
+		this.model.integrations.each(function(integration) {
+			var view = new IntegrationView({
+				model: integration
+			});
 
-      this._$integrations.append(view.$el);
-      view.render();
-    }, this);
-  }
+			this._$integrations.append(view.$el);
+			view.render();
+		}, this);
+	}
 });
 
 /*
 Provide the view to manage all the integrations
 */
 ConfiguredIntegrationManagerView = Backbone.View.extend({
-  initialize: function(options) {
-    this._$configuredIntegrations = null;
-    this.integrationID = options.integrationID;
-  },
+	initialize: function(options) {
+		this._$configuredIntegrations = null;
+		this.integrationID = options.integrationID;
+	},
 
-  render: function() {
-    this._$configuredIntegrations = this.$('.configured-integrations');
+	render: function() {
+		this._$configuredIntegrations = this.$('.configured-integrations');
 
-    this.listenTo(this.model, 'loaded', this._onLoaded);
+		this.listenTo(this.model, 'loaded', this._onLoaded);
 
-    this.model.load();
+		this.model.load();
 
-    return this;
-  },
+		return this;
+	},
 
-  _onLoaded: function() {
-    this.model.configuredIntegrations.each(function(configuredIntegration) {
-      var view = new ConfiguredIntegrationView({
-        model: configuredIntegration,
-        integrationID: this.integrationID
-      });
+	_onLoaded: function() {
+		this.model.configuredIntegrations.each(function(configuredIntegration) {
+			var view = new ConfiguredIntegrationView({
+				model: configuredIntegration,
+				integrationID: this.integrationID
+			});
 
-      this._$configuredIntegrations.prepend(view.$el);
-      view.render();
-    }, this);
-  }
+			this._$configuredIntegrations.prepend(view.$el);
+			view.render();
+		}, this);
+	}
 });
