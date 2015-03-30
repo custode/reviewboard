f1656f2503ade52ca9c328d139d6af06754eb4f1
xuanyi <xuanyi.custode@gmail.com>
2015-03-31 00:02:43 +0800

    Allow integration admin page to create a new integration

--- reviewboard/integrations/views.py
+++ reviewboard/integrations/views.py
@@ -2,11 +2,13 @@ from __future__ import unicode_literals
 
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.admin.views.decorators import staff_member_required
+from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 from reviewboard.integrations.manager import get_integration_manager
+from reviewboard.integrations.models import ConfiguredIntegration
 
 
 manager = get_integration_manager()
@@ -24,12 +26,23 @@ def integration_list(request,
 def configure_integration(request, integration_class=None, config_id=None,
                           template_name='integrations/configure_integration'
                                         '.html'):
+    if not integration_class:
+        integration_class = manager.get_config_instance(
+            int(config_id)).integration_id
+
+    integration = manager.get_integration(integration_class)
+
     if config_id:
         config_instance = manager.get_config_instance(int(config_id))
-        integration_class = config_instance.integration.integration_id
+    else:
+        config_instance = ConfiguredIntegration(
+            integration_id=integration_class,
+            is_enabled=False,
+            configuration=integration.default_configurations,
+            description=integration.description,
+            local_site=None)
 
-    if integration_class:
-        integration = manager.get_integration(integration_class)
+    integration = manager.get_integration(integration_class)
 
     if not integration:
         raise Http404
@@ -37,14 +50,21 @@ def configure_integration(request, integration_class=None, config_id=None,
         form_class = integration.form
 
     if request.method == 'POST':
-        form = form_class(integration, request.POST, request.FILES)
+        if not config_instance.pk:
+            manager.create_config(config_instance)
+            redirect_path = reverse('configure-integration',
+                                    args=(config_instance.pk,))
+        else:
+            redirect_path = request.path + '?save=1'
+
+        form = form_class(config_instance, request.POST, request.FILES)
 
         if form.is_valid():
             form.save()
 
-            return HttpResponseRedirect(request.path + '?save=1')
+            return HttpResponseRedirect(redirect_path)
     else:
-        form = form_class(integration, config=config_instance)
+        form = form_class(config_instance)
 
     return render_to_response(template_name, RequestContext(request, {
         'integration_name': integration.name,
