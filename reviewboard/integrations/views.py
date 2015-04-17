3444232e2020da816e32cb4437782d4cb5891efa
xuanyi <xuanyi.custode@gmail.com>
2015-04-18 02:57:25 +0800

    Fix review issues

--- reviewboard/integrations/views.py
+++ reviewboard/integrations/views.py
@@ -17,6 +17,10 @@ manager = get_integration_manager()
 @staff_member_required
 def integration_list(request,
                      template_name='integrations/integration_list.html'):
+    """Display a list of integrations class and its instances.
+
+    This page handles both the creation and configuration of integration.
+    """
     return render_to_response(template_name, RequestContext(request))
 
 
@@ -25,6 +29,10 @@ def integration_list(request,
 def configure_integration(request, integration_class=None, config_id=None,
                           template_name='integrations/configure_integration'
                                         '.html'):
+    """Display the configuration of a single integration instance.
+
+    This page handles the configuration of a specific integration instance.
+    """
     if config_id:
         config_instance = manager.get_config_instance(int(config_id))
         integration_class = config_instance.integration.integration_id
